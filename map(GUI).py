949726330b4ef6e9e714b1a6eff385866958020a
MrDJEnz <djenzmann@gmail.com>
2020-04-12 20:17:44 -0400

    Added a map we will use and separte countries.

---
+++ map(GUI).py
@@ -0,0 +1,26 @@
+import pygame
+import glob
+import  pickle
+
+
+#https://commons.wikimedia.org/wiki/File:Risk_board.svg
+#paths to draw islands
+
+
+
+class Colors():
+    def __init__(self):
+        self.red=(255,0,0)
+        self.green=(0,255,0)
+        self.blue=(0,0,255)
+        self.white=(255,255,255)
+        self.black=(0,0,0)
+        self.grey = (100, 100, 100)
+        self.yellow = (255, 255, 0)
+        self.purple = (255, 0, 255)
+        self.cian = (0, 255, 255)
+        self.dark_purple = (127, 0, 255)
+        self.dark_green = (0, 170, 0)
+        self.dark_red = (170, 0, 0)
+        self.dark_blue = (0, 0, 170)
+
